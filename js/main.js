0499f4517d2087eab32b42929d1bac2638b498ca
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-31 01:43:51 -0700

    Add mousedown, mouseleave, mouseup and mousemove events.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 (function(){
-    
+    const slider = document.querySelector('.items');
+    let isActive = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isActive = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.offsetLeft;
+        
+    });
+
+    slider.addEventListener('mouseleave', () => {
+        isActive = false;
+        slider.classList.remove('active');
+        
+    });
+
+    slider.addEventListener('mouseup', () => {
+        isActive = false;
+        slider.classList.remove('active');
+        
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if(!isActive) return;
+        e.preventDefault();
+    });
 }());
